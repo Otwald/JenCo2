def8cbfd1f93d49b3a718fb03bb35f8a556f5cbf
otwald <hinterlanddev@googlemail.com>
2021-09-26 09:14:17 +0200

    added rotue navigation in layout/nav

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  public items = [
+    {
+      route: 'welcome',
+      title: 'Willkommen',
+      name: 'Willkommen',
+      icon: 'home_icon',
+    },
+    {
+      route: 'round',
+      title: 'Spielrunden',
+      name: 'Spielrrunden',
+      icon: 'box_icon',
+    },
+    {
+      route: 'account',
+      title: 'Account-Verwaltung',
+      name: 'Account',
+      icon: 'user_icon',
+    },
+    {
+      route: 'admin',
+      title: 'Seiten-Verwaltung',
+      name: 'Admin-Bereich',
+      icon: '',
+    },
+    { route: 'login', title: 'Anmeldung', name: 'Anmeldung', icon: 'key_icon' },
+    {
+      route: 'logout',
+      title: 'Abmeldung',
+      name: 'Abmeldung',
+      icon: 'key_icon',
+    },
+  ];
+  constructor(private titleTagService: Title) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  /**
+   * sets the the Title of the Page in the Header
+   * @param pageTitle
+   */
+  public setTitle(pageTitle: string) {
+    this.titleTagService.setTitle(pageTitle);
   }
-
 }
